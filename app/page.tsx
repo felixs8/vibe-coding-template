e3701a1867f38fa0839296748339c721077edc11
Felix Schmidt <felix_schmidt_85@yahoo.de>
2025-08-22 17:31:16 +0100

    daisyui added

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,15 @@
 export default function Home() {
   return (
-    <main>
-      <h1>Hello World</h1>
-    </main>
+    <div className="hero bg-base-200 min-h-screen">
+      <div className="hero-content text-center">
+        <div className="max-w-md">
+          <h1 className="text-5xl font-bold">Hello World</h1>
+          <p className="py-6">
+            Vibe Coding Template with Next.js, Turbopack, Cypress and
+            TailwindCSS
+          </p>
+        </div>
+      </div>
+    </div>
   );
 }
